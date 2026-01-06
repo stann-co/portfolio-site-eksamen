dad1c91b4dae5d4367b8d210e1b32e158b42d919
Jack Conradsen <jackconradsen@gmail.com>
2026-01-06 13:05:12 +0100

    side navigation js virker

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,4 +1,34 @@
 let root = document.documentElement;
+let anchorLinks = document.querySelectorAll(".anchor-link");
+let anchorCircles = document.querySelectorAll(".circles > a");
+let activeAnchor = 0;
+
+function activeCircle(){
+	activeAnchor = -1;
+	anchorLinks.forEach(link => {
+		let linkPos = link.getBoundingClientRect().top;
+		if(linkPos - 300 < 0){
+			activeAnchor++;
+		}
+	});
+	activeAnchor = Math.max(activeAnchor, 0);
+	
+	anchorCircles.forEach((circle, index) => {
+		if(index === activeAnchor){
+			circle.classList.add("active");
+		} else {
+			circle.classList.remove("active");
+		}
+	});
+}
+
 document.addEventListener("scroll", evt => {
-	root.style.setProperty("--scrolltop", root.scrollTop);
+	let scroll = root.scrollTop;
+	root.style.setProperty("--scrolltop", scroll);
+
+	activeCircle();
 });
+
+window.onload = async (event) => {
+	activeCircle();
+};
