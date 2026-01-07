f5f2f06f7948404677e18832d34aa86cbf549796
Jack Conradsen <jackconradsen@gmail.com>
2026-01-07 07:44:40 +0100

    færdig!!!!

--- assets/js/general.js
+++ assets/js/general.js
@@ -22,13 +22,30 @@ function activeCircle(){
 	});
 }
 
+function parralaxBackgrounds(){
+	let lines = document.querySelector("#lines-pattern");
+	let circle1 = document.querySelector("#circle1-pattern");
+
+	let circleRotator = document.querySelector("#circle-rotator");
+
+	lines.setAttribute("y", root.scrollTop * -0.5);
+	circle1.setAttribute("y", root.scrollTop * -0.6);
+
+	circleRotator.setAttribute("transform", `rotate(${root.scrollTop * 0.05} 400 400)`);
+}
+
 document.addEventListener("scroll", evt => {
 	let scroll = root.scrollTop;
 	root.style.setProperty("--scrolltop", scroll);
 
 	activeCircle();
+	parralaxBackgrounds();
 });
 
 window.onload = async (event) => {
 	activeCircle();
 };
+
+function toggleDarkmode() {
+  document.documentElement.classList.toggle("dark-mode");
+} 
